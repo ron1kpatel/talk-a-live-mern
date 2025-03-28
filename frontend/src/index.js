c47a1e6cb7155eb9dda88214b8ac88f5fbedfa10
Ronak Patel <ronakpatel@Ronaks-MacBook-Air.local>
2025-03-28 16:16:24 +0530

    Fixed peer dependency Frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // <-- notice the new path
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,19 +7,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
 
-// Create the root using the new API
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <ChatProvider>
-          <App />
-        </ChatProvider>
-      </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>
+ReactDOM.render(
+  <ChakraProvider>
+    <BrowserRouter>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </BrowserRouter>
+  </ChakraProvider>,
+  document.getElementById("root")
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
